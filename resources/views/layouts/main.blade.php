7a57204d11b4ebd04363893c770054f84fbcd2d9
TheChosenOne29 <gilbertwuaten@gmail.com>
2022-04-06 23:04:54 +0700

    Fixed controller and links

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -72,10 +72,10 @@
 
 					<!-- nav -->
 					<ul class="main-nav nav navbar-nav">
-						<li><a href="index.html">Home</a></li>
-						<li><a href="index.html#about">About</a></li>
-						<li><a href="index.html#menu">Menu</a></li>
-						<li><a href="index.html#reservation">Reservation</a></li>
+						<li><a href="#home">Home</a></li>
+						<li><a href="#about">About</a></li>
+						<li><a href="#menu">Menu</a></li>
+						<li><a href="#reservation">Reservation</a></li>
 						<!-- <li><a href="index.html#gallery">Gallery</a></li> -->
 						<li><a href="index.html#events">Events</a></li>
 						<li><a href="index.html#contact">Contact</a></li>
