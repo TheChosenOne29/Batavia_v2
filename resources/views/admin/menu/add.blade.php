b02d034bfa7931b2fd969327999ecd4c459898f0
TheChosenOne29 <gilbertwuaten@gmail.com>
2022-04-29 18:43:16 +0700

    fixed add menu admin page

--- resources/views/admin/menu/add.blade.php
+++ resources/views/admin/menu/add.blade.php
@@ -8,22 +8,29 @@
         {{ csrf_field() }}
         <div class="mt-3 mb-3">
             <label for="form1" class="form-label">Menu Name</label>
-            <input class="form-control" id= "form1" type="text" name="name">
+            <input class="form-control" id= "form1" type="text" name="name" placeholder="Input item name">
           </div>
     
         <div class="mb-3">
           <label for="form2" class="form-label">Price</label>
-          <input class="form-control" id="form2" type="number" name="price">
+          <input class="form-control" id="form2" type="number" name="price" placeholder="Input item price">
         </div>
     
         <div class="mb-3">
             <label for="form3" class="form-label">Category</label>
-            <input class="form-control" id= "form3" type="text" name="category">
+            <select class="form-select" id="form3">
+                <option selected>Choose item category</option>
+                <option value="1">Light bites</option>
+                <option value="2">Appetizer</option>
+                <option value="3">Main Course</option>
+                <option value="4">Desserts</option>
+                <option value="5">Drinks</option>
+              </select>
         </div>
         
         <div class="mb-3">
             <label for="form4" class="form-label">Description</label>
-            <input class="form-control" id= "form4" type="text" name="desc">
+            <textarea class="form-control" id="form4" rows="3" placeholder="Input item description"></textarea>
         </div>
 
         <button type="submit" class="btn btn-primary">Submit</button>
